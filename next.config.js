45c644a51a02db292d455b5206871e56371fb50f
MihichN <91420816+MihichN@users.noreply.github.com>
2025-05-31 02:02:54 +0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/v1/create',
-        destination: '/v1/create',
-      },
-    ]
-  },
   // Отключаем строгую проверку типов для API роутов
   typescript: {
     ignoreBuildErrors: true,
